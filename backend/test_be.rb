7e18b99f42f8b2130b3d309e7d50da46f1c0b309
Danilo <vandor.danilo@gmail.com>
2015-05-17 06:14:01 -0300

    server working with session resume

--- backend/test_be.rb
+++ backend/test_be.rb
@@ -2,10 +2,6 @@ require 'net/http'
 require 'json'
 #require 'debug'
 
-def tesp(n)
-  res = Net::HTTP.get_response(URI($base + 'keepalive/'+ $cid.to_s))
-  puts "ping#{$user[n]}: #{res.code}"
-end
 
 def test_ping
   sleep(0.05)
@@ -14,33 +10,42 @@ def test_ping
 end
 
 def testf(n,f)
+  sleep(0.1)
   response = $http.post(f,
-                      {"user" => $user[n], "c_id" => $cid}.to_json,
+                      {"user" => $user[n], "c_id" => $cid, 'time' => 3}.to_json,
                       $json_headers)
-  puts response.body
+end
+
+def tesp(n)
+  #res = Net::HTTP.get_response(URI($base + 'keepalive/'+ $cid.to_s))
+  res = testf(n,'/keepalive')
+  puts "ping#{$user[n]}: #{res.body}"
 end
 
 $user = [1,3]
-uri = URI('http://localhost:4567/')
-$base = 'http://localhost:4567/'
+$base = 'http://172.22.38.148:4567/'
+#$base = 'http://hackvideo.herokuapp.com/'
+uri = URI($base)
 $json_headers ={"Content-Type" => "application/json", "Accept" => "application/json"}
 $http = Net::HTTP.new(uri.host, uri.port)
 
 #req = Net::HTTP::Post.new(
 #req.content_type = 'json'
 #req.body = {"users" => ["1","3"], "video" => '4'}.to_json
-5.times do
+5.times do |i|
   response = $http.post('/start',
-                        {"users" => ["1","3"], "video" => '4'}.to_json,
+                        {"users" => ["1","3"], "video" => i}.to_json,
                         $json_headers)
   $cid = JSON.parse(response.body)["session_id"]
   puts $cid
-  test_ping
-  testf(0,'/play')
-  test_ping
-  testf(1,'/play')
-  test_ping
-  testf(0,'/stop')
-  test_ping
-  res = Net::HTTP.get_response(URI($base + 'caguei/' + $cid.to_s))
+  #test_ping
+  #testf(0,'/play')
+  #test_ping
+  #testf(1,'/play')
+  #test_ping
+  #testf(1,'/seek')
+  #test_ping
+  #testf(0,'/play')
+  #test_ping
+  #res = Net::HTTP.get_response(URI($base + 'caguei/' + $cid.to_s))
 end
