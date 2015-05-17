4136158514a44e16829e3741d1f470289168936d
Danilo <vandor.danilo@gmail.com>
2015-05-16 23:45:57 -0300

    Finished the code for signaling server
    Methods implemented:
    /play 		<- Signals the user wants the server to play
    /stop 		<- Signals the user wants the server to pause
    /start 		<- Creates the session
    /caguei 	<- Deletes the session
    /keepalive 	<- Signals wether the others users are ready to play

--- backend/signaling.rb
+++ backend/signaling.rb
@@ -1,5 +1,110 @@
 require 'sinatra'
+#require 'sinatra/activerecord'
+#require './config/environments' #database configuration
+#require './models/model'        #Model class
+require 'json'
+require 'set'
+
+$connections = {}
+$conlist = []
+
+def tuple(params)
+  users,video = params["users"].sort,params["video"]
+  return "#{params["users"].join(",")} => #{params["video"]}"
+end
+
+def validate_json(data)
+  data && data['users'] && !data['users'].empty? && data['video']
+end
+
+def json_error(msg)
+  return {Error: msg}.to_json
+end 
 
 get '/' do
   'Noone should see this'
+end
+
+before do
+  request.body.rewind
+  body = request.body.read
+  @data= JSON.parse body unless body.empty?
+end
+
+#=> Post:
+#users: Arr(Str)    List of users IDs
+#video: Int         Video id to be watched
+#=> Return:
+#session_id : Int   Session id for the watch
+post '/start' do
+  #puts @data
+  #puts $connections.inspect
+  #puts $conlist.inspect
+  content_type :json
+  return json_error("empty data")  unless validate_json(@data)
+  state = tuple(@data)
+  return json_error("duplicate video")  if $connections[state]
+  num = ($conlist <<
+         ($connections[state] =
+          {rdy: Set.new(@data["users"]), time: 0, state: state})).size
+  #puts num.inspect
+  ##check if any user has not yet been registered
+  #users = [:user1,:user2].map{|u| data[u]}.map do |user|
+  #  u1= Users.find_or_create_by(uid: user)
+  #end
+  #State.create(video: data["video"], 
+  return {session_id: num-1}.to_json
+end
+
+#This method signals that the user is ready and waiting for the others
+#=> Post:
+#c_id: Int          Connection ID for the session
+#user: Int          User ID
+#=> Return
+#ack: Bool          Everything is OK
+post '/play' do
+  id,user = ["c_id","user"].map{|x| @data[x]}
+  #puts [id,user].inspect
+  return json_error("missing user") unless user
+  return json_error("video not registered") unless id && $conlist[id]
+  $conlist[id][:rdy].delete(user.to_s)
+  #puts $conlist[id][:rdy].inspect
+  #puts user
+  ncon = $conlist[id][:rdy]
+  return {ok: ncon.empty?}.to_json
+end
+
+#The user stopped the player
+#=> Post:
+#c_id: Int          Connection ID for the session
+#user: Int          User ID
+#=> Return
+#ack: Bool          Everything is OK
+post '/stop' do
+  id,user = ["c_id","user"].map{|x| @data[x]}
+  #puts [id,user].inspect
+  return json_error("missing user") unless user
+  return json_error("video not registered") unless id && $conlist[id]
+  $conlist[id][:rdy].add(user.to_s)
+  #puts $conlist[id][:rdy].inspect
+  #puts user
+  ncon = $conlist[id][:rdy]
+  return {ok: ncon.empty?}.to_json
+end
+#This method checks if everyone is ok with stuff playing
+get '/keepalive/:c_id' do
+  #puts "in keepalive"
+  #puts $conlist
+  return $conlist[params[:c_id].to_i][:rdy].empty? ? 200 : 406
+end
+
+get '/redirect/:c_cid/:uid' do
+end
+
+get '/caguei/:c_id' do
+  cid = params[:c_id].to_i
+  $connections.delete($conlist[cid][:state])
+  $conlist[cid] = nil
+  puts $conlist.inspect
+  puts $connections.inspect
 end
